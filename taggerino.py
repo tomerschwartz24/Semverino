d19c5fe550463973b3c8b541423484a668196ac5
tomerschwartz2411 <tomerschwartz2411@gmail.com>
2024-07-26 21:14:38 +0300

    added logger

--- taggerino.py
+++ taggerino.py
@@ -1,14 +1,15 @@
-
-from consts import INITAL_VERSION
 from determineo import check_if_tag_exist, check_if_previous_commit_is_tagged
+from helpers.logger import setup_logger
+from consts import INITAL_VERSION
 
+logger = setup_logger('taggerino-logger')
 
 def determine_if_tagging_is_needed():
     if check_if_tag_exist() is None and check_if_previous_commit_is_tagged() is None:
-        print("tagging needed, no tags were found on last and previous commit ")
+        logger.info("tagging needed, no tags were found on last and previous commit ")
         tagging_needed = True
     else:
-        print("tag was found, no tagging needed")
+        logger.warning("tag was found, no tagging needed")
         tagging_needed = False
     return tagging_needed
 

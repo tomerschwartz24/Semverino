35c3a0b527ec25417efc0e3ded1c9a0619461605
tomerschwartz2411 <tomerschwartz2411@gmail.com>
2024-07-26 09:50:36 +0300

    add test for whether tag exist or not

--- taggerino.py
+++ taggerino.py
@@ -1,33 +1,16 @@
-import subprocess
-import logging
 
 from consts import INITAL_VERSION
-
-logging.basicConfig(level=logging.DEBUG)
-
-
-def check_if_tag_exist():
-    try:
-        HEAD_TAG = subprocess.check_output(['git', 'describe', '--tags', '--abbrev=7', 'HEAD'], text=True)
-    except subprocess.CalledProcessError as no_tag_found:
-        logging.error(' It seems like the latest commit does not have any tags!')
-        if no_tag_found:
-            return None
-    if HEAD_TAG is not None:
-        logging.error(' Latest commit is tagged as: {}'.format(HEAD_TAG))
-        return HEAD_TAG
-    
-def check_if_previous_commit_is_tagged():
-    try:
-        PREVIOUS_TAG = subprocess.check_output(['git', 'describe', '--tags', 'HEAD~1'], text=True)
-    except subprocess.CalledProcessError as no_previous_tag_found:
-        logging.error(' It seems like the previous commit does not have any tags!')
-        if no_previous_tag_found:
-            return None
-    if PREVIOUS_TAG is not None:
-        logging.info(' Previous commit seems to be tagged as {}'.format(PREVIOUS_TAG))
-        return PREVIOUS_TAG
+from determineo import check_if_tag_exist, check_if_previous_commit_is_tagged
 
 
-check_if_previous_commit_is_tagged()
-# def determine_if_tagging_is_needed():
+def determine_if_tagging_is_needed():
+    if check_if_tag_exist() is None and check_if_previous_commit_is_tagged() is None:
+        print("tagging needed, no tags were found on last and previous commit ")
+        tagging_needed = True
+    else:
+        print("tag was found, no tagging needed")
+        tagging_needed = False
+    return tagging_needed
+
+
+tagging_needed = determine_if_tagging_is_needed()
